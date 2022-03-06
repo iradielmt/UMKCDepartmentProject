96bdc14f518b39e1802cfa71a41a86f656f61991
m-nweke <mnweke@mail.umkc.edu>
2022-03-06 15:38:38 -0600

    Backend portion of app

---
+++ gta-portal/index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+const api = express();
+
+api.listen(3000, () => {
+    console.log("Server started ...");
+});
+
+api.get("/", (req, res) => {
+    res.send("Hello From The Server");
+})
+
+api.use(bodyParser());
+
+api.post("/student", (req, res) => {
+});
+
+let fname = req.body.fname;
+let lname = req.body.lname;
+let id = req.body.id;
+
+function validateFName(fname) {
+    let errors = [];
+    if (fname.length == 0) {
+        errors.push("First Name Is Null");
+    }
+
+    if (fname.length > 50) {
+        errors.push("First Name Length Can Not Exceed 50 Characters.");
+    }
+    return errors;
+}
+
+function validateLName(lname) {
+    let errors = [];
+    if (lname.length == 0) {
+        errors.push("Last Name Is Null");
+    }
+
+    if (lname.length > 50) {
+        errors.push("Last Name Length Can Not Exceed 50 Characters.");
+    }
+
+    return errors;
+}
+
+function validateID(id) {
+    let errors = [];
+    // check whether ID is empty or not
+    if (id.length == 0) {
+        errors.push("ID Is empty");
+    }
+    // checks whether ID length is less then 10 character
+    if (id.length < 8) {
+        errors.push("ID must be at least 8 Digits");
+    }
+
+    // checks whether contact no length is more then 10 character
+    if (id.length > 9) {
+        errors.push("ID Must Be of 10 Digits");
+    }
+
+    // Using regular expression check whether contactno is only containing digits or not
+    if (!(/[0-9]/g.test(id))) {
+        errors.push("ID must contain digits only");
+    }
+
+    return errors;
+}

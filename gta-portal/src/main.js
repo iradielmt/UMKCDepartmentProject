fa68aea2288ea21c648eae2894084d2683a63289
BotZeroG <hawverg567@gmail.com>
2022-04-13 21:52:28 -0500

    Update main.js

--- gta-portal/src/main.js
+++ gta-portal/src/main.js
@@ -3,5 +3,6 @@ import App from '@/App.vue'
 import router from "./router"
 
 import 'bootstrap/dist/css/bootstrap.css';
+<style> @import 'src/assets/style.css'; </style>
 
 createApp(App).use(router).mount('#app');
